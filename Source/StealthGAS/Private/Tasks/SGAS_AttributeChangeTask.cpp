80da6d28e5cd5808334e36cd8b687ffa78c8352c
Emil Almazov <almazovemil@gmail.com>
2026-03-11 17:58:14 +0000

    create latent node for any attribute changes & GA for health changes + play death montage + prevent hit react GA from being applied once enemy is dead

---
+++ Source/StealthGAS/Private/Tasks/SGAS_AttributeChangeTask.cpp
@@ -0,0 +1,36 @@
+﻿// Copyright Emil Almazov
+
+
+#include "Tasks/SGAS_AttributeChangeTask.h"
+
+#include "AbilitySystemComponent.h"
+
+USGAS_AttributeChangeTask* USGAS_AttributeChangeTask::ListenForAttributeChange(UAbilitySystemComponent* AbilitySystemComponent,
+                                                                               FGameplayAttribute Attribute)
+{
+	if (!IsValid(AbilitySystemComponent)) return nullptr;
+	
+	USGAS_AttributeChangeTask* WaitForAttributeChangeTask = NewObject<USGAS_AttributeChangeTask>();
+	WaitForAttributeChangeTask->ACS = AbilitySystemComponent;
+	WaitForAttributeChangeTask->AttributeToListenFor = Attribute;
+	
+	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Attribute).AddUObject(WaitForAttributeChangeTask, &USGAS_AttributeChangeTask::HandelAttributeChanges);
+	
+	return WaitForAttributeChangeTask;
+}
+
+void USGAS_AttributeChangeTask::EndTask()
+{
+	if (ACS.IsValid())
+	{
+		ACS->GetGameplayAttributeValueChangeDelegate(AttributeToListenFor).RemoveAll(this);
+	}
+	
+	SetReadyToDestroy();
+	MarkAsGarbage();
+}
+
+void USGAS_AttributeChangeTask::HandelAttributeChanges(const FOnAttributeChangeData& AttributeChangeData)
+{
+	OnAttributeChanged.Broadcast(AttributeChangeData.Attribute, AttributeChangeData.NewValue, AttributeChangeData.OldValue);
+}
